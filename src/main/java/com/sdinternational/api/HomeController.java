6b802fa2e6f6e48c359621874667ddba694fabad
sm150820 <sm150820@student.sgh.waw.pl>
2026-05-25 09:43:33 +0000

    HomeController for root endpoint

---
+++ src/main/java/com/sdinternational/api/HomeController.java
@@ -0,0 +1,13 @@
+package com.sdinternational.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "TechCorp Game API is LIVE 🚀";
+    }
+}
